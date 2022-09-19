1f78b289c06730bba1480f76dd241ac81c0af668
lucyalvarado4692 <104829949+lucyalvarado4692@users.noreply.github.com>
2022-09-19 17:07:35 +0200

    test read logic test

---
+++ tests/Feature/Api/LogictestCrudTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Feature\Api;
+
+use App\Models\Logictest;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class LogictestCrudTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_check_If_Logictests_Are_Listed_In_Json_File(){
+      
+      Logictest::factory(2)->create();
+
+      $response = $this->get(route('logictestApi'));
+
+      $response->assertStatus(200)
+        ->assertJsonCount(2);
+
+    }
+}
