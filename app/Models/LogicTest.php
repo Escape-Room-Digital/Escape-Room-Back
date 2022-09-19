a9162cbb72833bae9afcb9c4a27b750ea8c02573
lucyalvarado4692 <104829949+lucyalvarado4692@users.noreply.github.com>
2022-09-19 12:00:06 +0200

    creando database

---
+++ app/Models/LogicTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class LogicTest extends Model
+{
+    use HasFactory;
+
+   protected $fillable=[
+    'name',
+    'statement',
+    'question',
+    'result',
+    'clue',
+    'image'
+   ];
+}
