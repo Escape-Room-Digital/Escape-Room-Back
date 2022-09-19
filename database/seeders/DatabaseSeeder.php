e9f03f89d61ef83d05a3e6d96aa8c33db045892b
lucyalvarado4692 <104829949+lucyalvarado4692@users.noreply.github.com>
2022-09-19 15:10:43 +0200

    read api logic test

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-       Logictest::factory(5)->create();
+       
+
+       Logictest::factory()->create(
+        ['name'=>'El caracol que cayó en el pozo',
+        'statement'=>'Un caracol cayó en un pozo de 15 metros. Cada día logra subir 3 metros, pero cada noche vuelve a caer 2.',
+        'question'=>'¿Cuánto tiempo demorará el caracol en salir del pozo?',
+        'result'=>'15 días',
+        'clue'=>'¿cuantos metros sube por dia?',
+        'image'=>'https://demascotas.info/wp-content/uploads/2021/07/pexels-capriauto-8154117-scaled-e1625822522143.jpg',]);
+      
+        Logictest::factory(1)->create();
 
     }
 }
