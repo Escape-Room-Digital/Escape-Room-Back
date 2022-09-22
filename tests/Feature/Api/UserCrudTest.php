6027e784ad116797ccec9254da04a1750a8e6a3d
lucyalvarado4692 <104829949+lucyalvarado4692@users.noreply.github.com>
2022-09-22 23:25:05 +0200

    test crud completo de userlist

--- tests/Feature/Api/UserCrudTest.php
+++ tests/Feature/Api/UserCrudTest.php
@@ -21,4 +21,68 @@ class UserCrudTest extends TestCase
         ->assertJsonCount(2);
 
     }
+
+     public function test_check_If_User_Are_deleted_In_Json_File(){
+
+      User::factory(2)->create();
+
+      $response = $this->get(route('userApi'));
+      $response->assertStatus(200)
+        ->assertJsonCount(2);
+
+      $response = $this->delete(route('destroyuserApi', 1));
+      
+      $response = $this->get(route('userApi'));
+      $response->assertStatus(200)
+        ->assertJsonCount(1);
+
+    }
+
+    public function test_check_if_a_user_can_be_created(){
+ 
+      $response=$this->post(route('storeuserApi',[
+            'name'=>'name',
+            'phone'=>'phone',
+            'email'=>'email',
+            'promo'=>'promo',
+            'solution'=>'solution',
+            'testdone'=>'testdone',
+            'password'=>'password',
+      ]));
+ 
+      $response = $this->get(route('userApi'));
+      $response->assertStatus(200)
+        ->assertJsonCount(1);
+ 
+    }
+
+     public function test_check_if_a_user_can_be_updated(){
+ 
+        $user = User::factory()->create();
+        $response = $this->get(route('userApi'));
+        $response->assertStatus(200)
+          ->assertJsonCount(1);
+  
+      $response=$this->patch(route('updateuserApi', $user->id), [
+            'name'=>'Gato',
+            'phone'=>'phone',
+            'email'=>'email',
+            'promo'=>'promo',
+            'solution'=>'solution',
+            'testdone'=>'testdone',
+            'password'=>'password',    
+        ]);
+  
+      $this->assertEquals('Gato', User::first()->name);
+   
+      }
+
+      public function test_check_if_a_user_can_be_show(){
+
+        $user = User::factory()->create();
+        $this->assertCount(1, User::all());
+        
+        $response=$this->get(route('showuserApi', $user->id));
+            $response->assertStatus(200)->assertSee("name")->assertJsonCount(1);
+      }
 }
